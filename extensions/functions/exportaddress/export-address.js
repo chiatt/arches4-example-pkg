09f0cb50de6cbef7d19bff36da363f0d6195d0c2
Cyrus Hiatt <chiatt@fargeo.com>
2018-04-29 05:59:58 -0700

    Fixes dirty state of target field/node mappings

--- extensions/functions/exportaddress/export-address.js
+++ extensions/functions/exportaddress/export-address.js
@@ -1,14 +1,42 @@
-define(['knockout',
+define(['jquery',
+        'underscore',
+        'knockout',
         'knockout-mapping',
         'views/list',
         'viewmodels/function',
         'bindings/chosen'],
-function (ko, koMapping, ListView, FunctionViewModel, chosen) {
+function ($, _, ko, koMapping, ListView, FunctionViewModel, chosen) {
     return ko.components.register('views/components/functions/export-address', {
         viewModel: function(params) {
             FunctionViewModel.apply(this, arguments);
             var self = this;
             this.external_address_url = params.config.external_address_url;
+            this.targetFields = params.config.target_fields;
+            this.targetFieldNames = _.map(this.targetFields(), function(field){
+                return field.name()
+            });
+            this.defaultNode = ko.observable(this.graph.nodes[0])
+            this.nodes = ko.observableArray();
+            this.graph.nodes.forEach(function(node){
+                this.nodes.push(node);
+            }, this);
+
+            $.ajax({
+                url: this.external_address_url() + '/?f=pjson'
+            }).done(function(data) {
+                _.each(JSON.parse(data).fields, function(field){
+                    if (field.editable && _.contains(this.targetFieldNames, field.name) === false) {
+                        fieldProperties = {'name': field.name, 'type': field.actualType, 'node': ko.observable('')}
+                        this.targetFields.push(fieldProperties);
+                        fieldProperties.node.subscribe(function(val){
+                            console.log(console.log('update triggering nodegroups here'))
+                        });
+                    }
+                }, self)
+
+            }).fail(function(err) {
+                console.log(err)
+            });
 
             this.graph.cards.forEach(function(card){
                 var found = !!_.find(this.graph.nodegroups, function(nodegroup){
@@ -19,7 +47,7 @@ function (ko, koMapping, ListView, FunctionViewModel, chosen) {
                 }
             }, this);
 
-            window.setTimeout(function(){$("select[data-bind^=chosen]").trigger("chosen:updated")}, 300);
+            window.setTimeout(function(){$("select[data-bind^=chosen]").trigger("chosen:updated")}, 1000);
         },
         template: {
             require: 'text!templates/views/components/functions/export-address.htm'
--- extensions/functions/exportaddress/export_address.py
+++ extensions/functions/exportaddress/export_address.py
@@ -14,12 +14,17 @@ details = {
     'name': 'Export Address',
     'type': 'node',
     'description': 'Just a sample demonstrating node group selection',
-    'defaultconfig': {"external_address_url":""},
+    'defaultconfig': {"external_address_url":"", "target_fields":[], "geometry_node":""},
     'classname': 'ExportAddress',
     'component': 'views/components/functions/export-address'
 }
 
 class ExportAddress(BaseFunction):
+
+    def __init__(self):
+         self.url = self.config['external_address_url']
+         self.geometry_node = self.config['geometry_node']
+         self.field_lookup = {field.node: field.name for field in self.config['target_fields']}
 
     def save(self, tile, request):
         tile_edits = json.loads(request.POST.get('data'))['data']
@@ -60,17 +65,6 @@ class ExportAddress(BaseFunction):
                 "token": "tTzVkJ7RPpZmqmlxc7xVBaORWK8vIKQenSkbmK13OnDfIHNKaNCIaH3i6Nz1AUbdnqkEsz8HuA-QqYrndP4yyqgov0NUgabK3lOO19erL-YYPtbIhEzahbSeQ0wPkJx1TH7RVL-gJ9m3iBsV9Affr0NczrLunSdj6rsa1Kg4QI8fTWpdgj0VCy7FaANWggjI6b7kDATtb43W9-hHxmndcjEU9S7lBzCfTty1b4GnAF3dmYhoh4ZBLC-XpsLetKEJ"
             }
 
-            field_lookup = {
-                "29862afe-4746-11e8-88b1-0242ac120006": "Block_Lot",
-                "1a08f610-4746-11e8-b7cc-0242ac120006": "Street Name",
-                "1a08fbd8-4746-11e8-b7cc-0242ac120006": "Address_Number",
-                "1a08f3cc-4746-11e8-b7cc-0242ac120006": "Address_Number_Suffix",
-                "1a08f80e-4746-11e8-b7cc-0242ac120006": "CNN",
-            }
-
-            geometry_node = '2ad20702-4746-11e8-a9a0-0242ac120006'
-
-
             result_node = models.Node.objects.get(pk='1a08f3cc-4746-11e8-b7cc-0242ac120006')
             external_reference = Tile.objects.filter(nodegroup=result_node.nodegroup).filter(resourceinstance=tile.resourceinstance_id)
             tiles = Tile.objects.filter(resourceinstance=tile.resourceinstance_id)
@@ -79,25 +73,24 @@ class ExportAddress(BaseFunction):
 
             for tile in tiles:
                 for tile_node, tile_value in tile.data.iteritems():
-                    # if models.Node.objects.get(pk=tile_node).datatype == 'geojson-feature-collection':
-                    if tile_node == geometry_node:
+                    if tile_node == self.geometry_node:
                         geom = GEOSGeometry(json.dumps(tile_value['features'][0]['geometry']), srid=4326)
                         geom.transform(3857)
                         address['geometry']['x'] = geom.x
                         address['geometry']['y'] = geom.y
                         has_geom = True
-                    if tile_node in field_lookup:
-                        address["attributes"][field_lookup[tile_node]] = str(tile_value)
+                    if tile_node in self.field_lookup:
+                        address["attributes"][self.field_lookup[tile_node]] = str(tile_value)
 
             for edit_node, edit_value in tile_edits.iteritems():
-                if edit_node == geometry_node:
+                if edit_node == self.geometry_node:
                     geom = GEOSGeometry(json.dumps(edit_value['features'][0]['geometry']), srid=4326)
                     geom.transform(3857)
                     address['geometry']['x'] = geom.x
                     address['geometry']['y'] = geom.y
                     has_geom = True
-                if edit_node in field_lookup:
-                    address["attributes"][field_lookup[edit_node]] = str(edit_value)
+                if edit_node in self.field_lookup:
+                    address["attributes"][self.field_lookup[edit_node]] = str(edit_value)
 
             if has_geom:
                 if len(external_reference) != 0:
